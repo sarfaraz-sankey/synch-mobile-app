e65e319be349250d8f82220c2230cdf858dd4bb4
sarfaraz ali <sarfaraz.a@sankeysolutions.com>
2025-12-01 10:53:57 +0530

    setup: tailwindcss setup

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,7 +6,7 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p className='bg-red-400'>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
 };
